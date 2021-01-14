b234270e3d4d96054a93cbad3735b9f6d8e8ab40
revmat56 <62846638+revmat56@users.noreply.github.com>
2021-01-14 13:50:16 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -4,23 +4,23 @@
 // tutaj definicja klasy Makaron
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
 
-class Makaron {
-public:
-	virtual double ileMaki(unsigned) const {};
-	static Makaron* gotujMakaron(std::string& s);
-	~Makaron()
-	{
-		delete ptr;
-	}
+//class Makaron {
+//public:
+//	virtual double ileMaki(unsigned) const {};
+//	static Makaron* gotujMakaron(std::string& s);
+//	~Makaron()
+//	{
+//		delete ptr;
+//	}
 	
-private:
-	static Makaron* ptr;
-};
+//private:
+//	static Makaron* ptr;
+//};
 
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
-class Tagliatelle : public Makaron {
+class Tagliatelle  {
 
 public:
 	Tagliatelle(double l, double w, double r) : L(l), W(w), R(r)	{}
