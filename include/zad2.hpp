37d7b47b9693cd03926170f559e2a575ff1017e3
revmat56 <62846638+revmat56@users.noreply.github.com>
2021-01-14 13:27:25 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -6,4 +6,32 @@
 
 // tutaj klasa Penne
 
+class Penne : public Makaron {
+	
+public:
+	double ileMaki(unsigned P) const
+	{
+		return P;
+	}
+
+};
+
 // tutaj definicja metody gotujMakaron
+
+Makaron* gotujMakaron(std::string& s)
+	{
+		if (s.front() == s.back())
+		{
+			 ptr = new Tagliatelle(3.12,0.42,0.1);
+			return ptr;
+		}
+		else
+		{
+			ptr = new Penne();
+			return ptr;
+		}
+	};
+	~Makaron()
+	{
+		delete ptr;
+	}
