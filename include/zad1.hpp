b53451de9476db779f46ee55d0544ef6a86cc596
revmat56 <62846638+revmat56@users.noreply.github.com>
2021-01-14 12:26:48 +0100

    Update zad1.hpp
    a1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -6,3 +6,26 @@
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
+class Tagiatelle {
+
+public:
+	Tagiatelle(double l, double w, double r) : L(l), W(w), R(r)	{}
+	Tagiatelle()
+	{
+		L = 0.5;
+		W = 0.5;
+		R = 0.5;
+	}
+	double ileMaki(unsigned P) const
+	{
+		return P * L * W * (1.0 - R) * C;
+	}
+
+private: 
+	double L;
+	double W;
+	double R;
+
+	static const double C;
+
+};
